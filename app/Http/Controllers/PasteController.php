52b9f2187255dcff3c0404db5bc6c53b60437f27
Laralinks <richard@laravel.link>
2020-09-27 14:56:06 -0400

    test

--- app/Http/Controllers/PasteController.php
+++ app/Http/Controllers/PasteController.php
@@ -9,7 +9,8 @@ use Inertia\Inertia;
 class PasteController extends Controller
 {
     public function index() {
-        $pastes = Paste::where(['private' => false, 'password' => null])->latest()->paginate(5);
-        return Inertia::render('Paste', ['pastes', $pastes]);
+        //$pastes = Paste::where(['private' => false, 'password' => null])->latest()->paginate(5);
+        $pastes = Paste::all();
+        return Inertia::render('Paste', ['pastes' => $pastes]);
     }
 }
