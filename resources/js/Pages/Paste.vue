5efe4ba10902b265c8e39010a128fc6af2a316a8
Laralinks <richard@laravel.link>
2020-09-27 15:18:34 -0400

    test

--- resources/js/Pages/Paste.vue
+++ resources/js/Pages/Paste.vue
@@ -8,7 +8,6 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
                     <div class="py-1">
                         <div class=" min-w-full mx-auto sm:px-6 lg:px-8">
@@ -18,17 +17,16 @@
                                         Latest Pastes
                                     </div>
                                 </div>
-                                <div class="" v-for="paste in pastes">
-                                    <div
-                                        class="{{'bg-gray-200'}} bg-opacity-25 min-w-full px-10 py-6 border-b border-gray-200 md:border-t-0 md:border-l">
+                                <div class="">
+                                    <div class="{{'bg-gray-200'}} bg-opacity-25 min-w-full px-10 py-6 border-b border-gray-200 md:border-t-0 md:border-l" v-for="paste in pastes">
                                         <div class="justify-between items-center flex">
-                                            <span class="font-light text-gray-600">date/time</span>
+                                            <span class="font-light text-gray-600">{{ paste.created_at }}</span>
                                             <a class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500"
                                                href="#">Language</a>
                                         </div>
                                         <div class="mt-2">
-                                            <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#">Title</a>
-                                            <p class="mt-2 text-gray-600"><code>Description</code></p>
+                                            <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" href="#">{{ paste.title }}</a>
+                                            <p class="mt-2 text-gray-600"><code>{{ paste.description }}</code></p>
                                         </div>
                                         <div class="flex justify-between items-center mt-4">
                                             <a class="text-blue-600 hover:underline" href="#">View Paste</a>
@@ -36,8 +34,8 @@
                                                 <a class="flex items-center" href="#">
                                                     <img
                                                         class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
-                                                        src="#" alt="avatar">
-                                                    <h1 class="text-gray-700 font-bold">name</h1>
+                                                        src="{{ paste.user.profile_photo_url }}" alt="avatar">
+                                                    <h1 class="text-gray-700 font-bold">{{ paste.user.name }}</h1>
                                                 </a>
                                             </div>
                                         </div>
@@ -52,7 +50,6 @@
                         </div>
                     </div>
 
-                </div>
             </div>
         </div>
     </app-layout>
